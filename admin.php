a6dd4c801e3f8f110ee6566246db71dd539b52e6
Andri <a.halili2004@gmail.com>
2024-12-25 15:35:49 +0100

    Added border outline in logo

---
+++ admin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kits Alb - Admin Dashboard</title>
+    
+    <!-- Link to the header-specific CSS file -->
+    <link rel="stylesheet" href="styles/shared/kits-header.css">
+    
+    <!-- Link to the Admin-specific CSS file -->
+    <link rel="stylesheet" href="styles/admin/admin.css">
+</head>
+<body>
+    <header class="kits-header">
+        <!-- Left Section: Logo and Branding -->
+        <section class="left-section">
+            <a href="admin_dashboard.php" class="header-link">
+                <img class="kits-logo" src="images/kits-logo-white.png" alt="Kits Alb Logo">
+                <img class="kits-mobile-logo" src="images/kits-mobile-logo-white.png" alt="Kits Alb Mobile Logo">
+            </a>
+        </section>
+        
+
+</body>
+</html>

e10ee45000074a85d616e627f70e4dd04cf49478
Andri <a.halili2004@gmail.com>
2024-12-25 13:02:41 +0100

    Added contact page and fixed some small details

---
+++ contact.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact Us</title>
+  <link rel="stylesheet" href="styles/pages/contact.css">
+  <link rel="stylesheet" href="styles/pages/login.css">
+</head>
+<body>
+    <header>
+        <a href="index.php" class="logo">
+            <img src="images/kits-logo-white.png" alt="Kits.alb Logo">
+        </a>
+    </header>
+
+  <div class="new_home_web">
+    <div class="responsive-container-block big-container">
+      <div class="responsive-container-block textContainer">
+        <div class="topHead">
+          <p class="text-blk heading">
+            Get in
+            <span class="yellowText">
+              touch
+            </span>
+          </p>
+          <div class="yellowLine" id="w-c-s-bgc_p-2-dm-id">
+          </div>
+        </div>
+        <p class="text-blk subHeading">
+         Have questions or feedback? We’d love to hear from you!
+        </p>
+      </div>
+      <div class="responsive-container-block container">
+        <div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-7 wk-ipadp-10 line" id="i69b">
+          <form class="form-box">
+            <div class="container-block form-wrapper">
+              <div class="responsive-container-block">
+                <div class="left4">
+                  <div class="responsive-cell-block wk-ipadp-6 wk-tab-12 wk-mobile-12 wk-desk-6" id="i10mt-2">
+                    <input class="input" id="ijowk-2" name="FirstName" placeholder="First Name">
+                  </div>
+                  <div class="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
+                    <input class="input" id="indfi-2" name="Last Name" placeholder="Last Name">
+                  </div>
+                  <div class="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
+                    <input class="input" id="ipmgh-2" name="Email" placeholder="Email Address">
+                  </div>
+                  <div class="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12 lastPhone">
+                    <input class="input" id="imgis-2" name="PhoneNumber" placeholder="Phone Number">
+                  </div>
+                </div>
+                <div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="i634i-2">
+                  <textarea class="textinput" id="i5vyy-2" placeholder="Message"></textarea>
+                </div>
+              </div>
+              <input type = "submit" name = "submit" class="send"  id="w-c-s-bgc_p-1-dm-id">
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer class="kits-footer">
+      <p>&copy; 2024 Football Kits Albania <br> Follow us on 
+        <a href="https://instagram.com/kits.alb" target="_blank" class="footer-link">Instagram</a>
+      </p>
+    </footer>
+    
+</body>
+</html>
